e8fc2820a4eeb030a80c10116159c7352e5c2ab8
Artur Faruga <farugaa.training@gmail.com>
2017-11-15 17:24:17 +0100

    enable h2 console and hibernate logs2233

--- src/main/java/pl/akademiakodu/database/DatabaseApplication.java
+++ src/main/java/pl/akademiakodu/database/DatabaseApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import pl.akademiakodu.database.model.onetomany.Product;
 import pl.akademiakodu.database.model.onetomany.ShoppingCart;
-import pl.akademiakodu.database.repository.ShoppingCartRepository;
 import pl.akademiakodu.database.service.ProductService;
 import pl.akademiakodu.database.service.ShoppingCartService;
 
@@ -25,27 +24,32 @@ public class DatabaseApplication {
 	public CommandLineRunner runWithDb(ShoppingCartService shoppingCartService, ProductService productService){
 		return (args -> {
 
-			ShoppingCart koszyk1 = new ShoppingCart("koszyk1");
-
-			Product mleko = new Product("mleko", koszyk1);
-			Product chleb = new Product("chleb", koszyk1);
-			Product maslo =  new Product("maslo", koszyk1);
-
-			List<Product> products =new ArrayList<>();
-			products.add(mleko);
-			products.add(chleb);
-			products.add(maslo);
-
-			koszyk1.setProducts(products);
+			ShoppingCart koszyk1 = buildShoppingCartWithProducts("koszyk1", "mleko", "chleb", "maslo");
+			ShoppingCart koszyk2 = buildShoppingCartWithProducts("koszyk2", "bmw", "audi", "masertti");
 
 			shoppingCartService.save(koszyk1);
-			/*productService.save(mleko);
-			productService.save(chleb);
-			productService.save(maslo);*/
+			shoppingCartService.save(koszyk2);
+
 
 
 
 		});
+	}
+
+	private ShoppingCart buildShoppingCartWithProducts(String koszyk11, String product1Name, String product2Name, String product3Name) {
+		ShoppingCart koszyk1 = new ShoppingCart(koszyk11);
+
+		Product mleko = new Product(product1Name, koszyk1);
+		Product chleb = new Product(product2Name, koszyk1);
+		Product maslo =  new Product(product3Name, koszyk1);
+
+		List<Product> products =new ArrayList<>();
+		products.add(mleko);
+		products.add(chleb);
+		products.add(maslo);
+
+		koszyk1.setProducts(products);
+		return koszyk1;
 	}
 
 }
